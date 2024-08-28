e4e508f90173a5d2067677f577a60a0d1fb69a48
DheZhengShi <trolljoe42069@gmail.com>
2024-08-28 18:57:10 -0500

    Update bot.py
    Some new commands added and fixed

--- bot.py
+++ bot.py
@@ -1,36 +1,51 @@
 import discord
 from discord.ext import commands
 import passwen
+import os, random, requests
+#import Extensions.cntrs as ab
+
+#this indicates old code
+#client = discord.Client(intents=intents)
+#@client.event
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="&&", intents=intents)
 
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+###
+
 async def on_ready():
-    print(f"Hemos iniciado sesión como {bot.user}")
+  print(f"Bot loggued in as {bot.user}")
 
 @bot.command(name = "hi")
 async def hi(ctx, nombre):
     await ctx.send(f"Hello, my name is: {bot.user.name}, I'm pleasure to talk with ya' {nombre}")
 
-@bot.command(name1 = "passgen")
-async def passgen(ctx, i:int):
-  await passwen.gen_pass(i)
+@bot.command(name = "passgen")
+async def passgen(ctx, i):
+  await ctx.send(passwen.gen_pass(int(i)))
     
+@bot.command(name = "weed")
+async def weed(ctx):
+   with open("Discord/random_images/weed.jpg","rb") as img:
+      picture = discord.File(img)
+   await ctx.send(file = picture)
 
-bot.run("")
+@bot.command(name = "img")
+async def randomimg(ctx):
+   j = random.choice(os.listdir("discord/random_images"))
+   with open(f"discord/random_images/{j}","rb") as img:
+      picture = discord.File(img)
+   await ctx.send(file = picture)
 
-#this is old code
-#client = discord.Client(intents=intents)
-#@client.event
-  
-#async def on_message(message):
+@bot.command(name = "duck")
+async def ducky(ctx):
+  img1 = get_duck_image_url()
+  await ctx.send(img1)
 
- #   if message.author == client.user :
-  #      return
-   # if message.content.startswith("!hello"):
-  #      await message.channel.send("HI!!!!11!11") 
-   # elif message.content.startswith('!bye'):
-  #      await message.channel.send("I'm sad by hearing that...")
-   # else:
-    #    await message.channel.send(message.content)
+bot.run(TOKEN)
